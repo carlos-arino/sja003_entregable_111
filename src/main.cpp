02070d7161dda57f9ee7a80f77a6f0c8a4234e49
carlos-arino <arino@uji.es>
2025-12-14 23:53:26 +0100

    Actualizar el archivo main.cpp para reflejar el ejercicio entregable 111 y cambiar la funcionalidad de lectura de luminosidad a temperatura.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-/* Ejercicio entregable 011
+/* Ejercicio entregable 111
  Deben de utilizar este archivo los alumnos con c<5, d<5, u>=5
  siendo c,d,u las tres últimas cifras del DNI 22000cdu -W
- Luminosidad y motor continua con potenciometro
- Para cambiar Luminosidad o haz click sobre el sensor NTC durante la simulacion
+CSensor de temperatura y motor continua con potenciometro
+ Para cambiar la temperatura o haz click sobre el sensor durante la simulacion
  rellenar vuestro nombre y DNI
  NOMBRE ALUMNO: XXXXX
  DNI: XXXXX
@@ -10,9 +10,14 @@
 */
 
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-const int luxPin = 35; // Pin analógico para el sensor de luminosidad
+const int tempPin = 4; // Pin del sensor de temperatura
 const int potPin = 34; // Pin analógico para el sensor de posición
+
+OneWire oneWire(tempPin);
+DallasTemperature sensors(&oneWire);
 
 void setup()
 {
@@ -21,12 +26,16 @@ void setup()
   Serial.println("Hello, ESP32!");
   ledcAttachPin(26, 0);
   ledcSetup(0, 1000, 10); // frecuencia 1KHz y resolucion 2^10
+  sensors.begin();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   delay(10); // this speeds up the simulation
+  sensors.requestTemperatures();
+  const float tempC = sensors.getTempCByIndex(0);
+
   if (millis() < 5000)
     ledcWrite(0, 200);
   if (millis() > 5000)
@@ -35,7 +44,7 @@ void loop()
     ledcWrite(0, 1000);
   Serial.print(analogRead(potPin));
   Serial.print(", ");
-  Serial.print(analogRead(luxPin));
+  Serial.print(tempC);
   Serial.println(" ");
   delay(100);
 }
